16d1a285aaf1ef3582f0320fab1c82edf9387bf6
Devender Kumar <lucdev93@gmail.com>
2025-01-18 00:49:51 +0530

    build with vue

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Hello, Vue.js in Laravel with Vite!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ExampleComponent",
+};
+</script>
+
+<style scoped>
+h1 {
+    color: green;
+}
+</style>
