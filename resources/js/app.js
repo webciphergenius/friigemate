d11a981cb3ce1ca6b9c34213addea641bb557efe
Devender Kumar <lucdev93@gmail.com>
2025-01-18 00:10:43 +0530

    install and confure vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+
+const app = createApp({});
+
+// Example: Add a Vue component
+import ExampleComponent from "./components/ExampleComponent.vue";
+app.component("example-component", ExampleComponent);
+
+app.mount("#app");
