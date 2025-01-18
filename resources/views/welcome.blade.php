1adc422972a7da397aaedcd8adcf32cfd7bc1b58
Devender Kumar <lucdev93@gmail.com>
2025-01-18 19:28:41 +0530

    move build to root

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel + Vue + Vite</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+   
 </head>
 <body>
     <div id="app">
