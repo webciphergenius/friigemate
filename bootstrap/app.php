3140c60edf8404daf8ab0cc11a9fe2f48d96441e
Devender Kumar <lucdev93@gmail.com>
2025-03-24 22:28:15 +0530

    fixing the image upload issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
