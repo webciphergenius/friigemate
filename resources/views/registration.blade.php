6a4eb28a2a6c7048ec3f48a1935781f3dc64461c
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-06-11 20:53:20 +0530

    Include CSS file under register form

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Registration</title>
     @vite('resources/js/app.js') <!-- Or mix if you're using Mix -->
+    @vite('resources/css/app.css')
 </head>
 <body>
     <div id="app">
