7049016b3dc1af6813853adf3365a1543bff2047
Devender Kumar <lucdev93@gmail.com>
2025-06-11 19:44:29 +0530

    adding the form on the website

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,13 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
+import RegistrationForm from "./components/RegistrationForm.vue";
 import HomePage from "./components/HomePage.vue";
 
 const app = createApp({
     components: {
         HomePage,
+        RegistrationForm,
     },
 });
 
---
+++ resources/views/registration.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration</title>
+    @vite('resources/js/app.js') <!-- Or mix if you're using Mix -->
+</head>
+<body>
+    <div id="app">
+        <registration-form></registration-form>
+    </div>
+</body>
+</html>
