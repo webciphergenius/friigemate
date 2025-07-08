4b85e9d261e7f8a7d0823ab8a83d08f83df4aeef
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-07-08 16:33:17 +0530

    Import privacyPolicy

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import RegistrationForm from "./components/RegistrationForm.vue";
 import RegistrationDriver from "./components/RegistrationDriver.vue";
-import PrivacyPolicy from "./components/privacyPolicy.vue";
+import privacyPolicy from "./components/privacyPolicy.vue";
 import HomePage from "./components/HomePage.vue";
 
 const app = createApp({
@@ -11,7 +11,7 @@ const app = createApp({
         HomePage,
         RegistrationForm,
         RegistrationDriver,
-        PrivacyPolicy,
+        privacyPolicy,
     },
 });
 
