29769b2f8517b46ff7a92a77bd2fe058f130730f
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-07-09 00:36:39 +0530

    Solution

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,13 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import PrivacyPolicy from './components/PrivacyPolicy.vue'
+
+import PrivacyPolicy from './components/PrivacyPolicy.vue';
 import HomePage from "./components/HomePage.vue";
 
-const app = createApp({
-    components: {
-        HomePage,
-        PrivacyPolicy
-    },
-});
+const app = createApp({});
+
+app.component('privacy-policy', PrivacyPolicy);
+app.component('home-page', HomePage);
 
 app.mount("#app");
--- resources/js/components/privacyPolicy.vue
+++ resources/js/components/privacyPolicy.vue
@@ -1,1 +1,11 @@
-<p>Test Render</p>
+<template>
+  <div>
+    <p>Test Render</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrivacyPolicy'
+}
+</script>
