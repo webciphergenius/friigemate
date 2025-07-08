82e1fc031004f1d826c10ba109772d7beb960d56
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-07-09 00:39:38 +0530

    Revert App.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,14 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-import PrivacyPolicy from './components/PrivacyPolicy.vue';
+import PrivacyPolicy from './components/PrivacyPolicy.vue'
 import HomePage from "./components/HomePage.vue";
 
-const app = createApp({});
-
-app.component('privacy-policy', PrivacyPolicy);
-app.component('home-page', HomePage);
+const app = createApp({
+    components: {
+        HomePage,
+        PrivacyPolicy
+    },
+});
 
 app.mount("#app");
