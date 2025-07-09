b6239de5c10bd8238e5c64e492427027212def35
codebeelabpvtltd <codebeelabpvtltd@gmail.com>
2025-07-09 23:35:53 +0530

    fixing the error on policy page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import PrivacyPolicy from './components/PrivacyPolicy.vue'
+import PrivacyPolicy from './components/privacyPolicy.vue'
 import HomePage from "./components/HomePage.vue";
 
 const app = createApp({
