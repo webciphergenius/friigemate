0155a80ee871745bd84e44d941d0ca9bad840b3b
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-07-10 00:14:58 +0530

    Designed Policy page

--- resources/js/components/privacyPolicy.vue
+++ resources/js/components/privacyPolicy.vue
@@ -1,11 +1,18 @@
 <template>
 <div class="privacy-policy container mx-auto p-6 text-gray-800">
-<h1 class="text-3xl font-bold mb-4">Privacy Policy</h1>
+<HeaderSection />
+<div class="privacy-policy-head text-center">
+    <h1 class="text-3xl font-bold mb-4">Our Privacy Policy</h1>
+    <p class="mb-4">
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet
+    </p>
+</div>
+<h2 class="text-3xl font-bold mb-4">Privacy Policy</h2>
 <p class="mb-4"> 
       This privacy policy sets out how Hayek's Market uses and protects any information that you give COMPANY when you use this website. COMPANY is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using  this website, then you can be assured that it will only be used in accordance with this privacy statement. COMPANY may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.
 </p>
  
-    <h2 class="text-xl font-semibold mt-6 mb-2">WHAT WE COLLECT</h2>
+    <h3 class="text-xl font-semibold mt-6 mb-2">WHAT WE COLLECT</h3>
 <p class="mb-4">
 <b>
         We may collect the following information:
@@ -33,22 +40,22 @@
 <li>We may use the information to customize the website according to your interests.</li>
 </ul>
  
-    <h2 class="text-xl font-semibold mt-6 mb-2">SECURITY</h2>
+    <h3 class="text-xl font-semibold mt-6 mb-2">SECURITY</h3>
 <p class="mb-4">
       We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.
 </p>
  
-    <h2 class="text-xl font-semibold mt-6 mb-2">HOW WE USE COOKIES</h2>
+    <h3 class="text-xl font-semibold mt-6 mb-2">HOW WE USE COOKIES</h3>
 <p class="mb-4">
       A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree, the file is added, and the cookie helps analyze web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences. We use traffic log cookies to identify which pages are being used. This helps us analyze data about webpage traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system. Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us. You can choose to accept or decline cookies. Most web browsers  accept cookies, but you can usually modify your browser setting to decline cookies if you prefer. This may prevent you from taking full advantage of the website.
 </p>
  
-    <h2 class="text-xl font-semibold mt-6 mb-2">LINKS TO OTHER WEBSITES</h2>
+    <h3 class="text-xl font-semibold mt-6 mb-2">LINKS TO OTHER WEBSITES</h3>
 <p class="mb-4">
       Our website may contain links to other websites of interest. However, once you have used these links to leave our site, you should note that we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the website in question.
 </p>
  
-    <h2 class="text-xl font-semibold mt-6 mb-2">CONTROLLING YOUR PERSONAL INFORMATION</h2>
+    <h3 class="text-xl font-semibold mt-6 mb-2">CONTROLLING YOUR PERSONAL INFORMATION</h3>
 <p class="mb-4">
 <b>
         You may choose to restrict the collection or use of your personal information in the following ways: 
@@ -64,10 +71,19 @@
       We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen. You may request details of personal information which we hold about you under the Data Protection Act 1998. A small fee will be payable. If you would like a copy of the information held on you, please contact us. If you believe that any information, we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.
 </p>
 </div>
+<footerSection />
 </template>
  
 <script>
+import HeaderSection from '@/components/HeaderSection.vue'
+import footerSection from '@/components/footerSection.vue'
+
 export default {
-  name: 'privacyPolicy'
+  name: 'privacyPolicy',
+  components: {
+    HeaderSection,
+    footerSection
+  }
 }
+
 </script>
