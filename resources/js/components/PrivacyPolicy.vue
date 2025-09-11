e9345964fb73a1ffa403f61896ed9f3957c8ff9a
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-09-11 13:11:35 +0530

    Changes in policy file added

---
+++ resources/js/components/PrivacyPolicy.vue
@@ -0,0 +1,249 @@
+<template>
+  <header class="mainHead">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
+        <div class="container">
+            
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid">
+                  <a class="navbar-brand fw-bold" href="/"><img src="../../images/Go_FreightMate_logo.png" alt="brand-logo"></a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="#home" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Home
+                            </a>
+                           
+                      </li>
+                     
+                      <li class="nav-item">
+                        <a class="nav-link" href="#screen">Screens</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#feature">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#team">Our Team</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#myblg">Blog</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#footer">Contact</a>
+                      </li>
+                    </ul>
+                    <ul class="list-inline socialLinks">
+                        <li class="list-inline-item"><a href="#"><img src="../../images/facebook.png" alt="social-icon"></a></li>
+                        <li class="list-inline-item"><a href="#"><img src="../../images/youtube.png" alt="social-icon"></a></li>
+                        <li class="list-inline-item"><a href="#"><img src="../../images/insta.png" alt="social-icon"></a></li>
+                        <li class="list-inline-item"><a href="#"><img src="../../images/tiktok.png" alt="social-icon"></a></li>
+                    </ul>
+                    <a href="/registration" class="getstartedBtn">Get Started</a>
+                </div>
+            </div>
+              </nav>
+        </div>
+    </header>
+
+    <div class="container py-5">
+        <div class="terms-header text-center mb-4">
+            <h1>OUR PRIVACY POLICY</h1>
+            <h2>Privacy Policy</h2>
+        </div>
+        <div class="terms-content">
+            <p>This privacy policy sets out how Go Freightmate® uses and protects any information that you give Go Freightmate® when you use this website. Go Freightmate® is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using  this website, then you can be assured that it will only be used in accordance with this privacy statement. Go Freightmate® may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes.</p>
+
+            <h3>WHAT WE COLLECT</h3>
+            <h4>We may collect the following information:</h4>
+            <ul>
+                <li>Name and job title.</li>
+                <li>Contact information including email address.</li>
+                <li>Demographic information such as postcode, preferences and interests.</li>
+                <li>Other information relevant to customer surveys and/or offers.</li>
+            </ul>
+
+            <h3>What we do with the information we gather:</h3>
+            <p>We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:</p>
+            <ul>
+                <li>Internal record keeping.</li>
+                <li>We may use the information to improve our products and services.</li>
+                <li>We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.</li>
+                <li>From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail.</li>
+                <li>We may use the information to customize the website according to your interests.</li>
+            </ul>
+
+            <h3>SECURITY</h3>
+            <p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure we have put in place suitable physical, electronic and managerial procedures to safeguard and secure the information we collect online.</p>
+
+            <h3>HOW WE USE COOKIES</h3>
+            <p>A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree, the file is added, and the cookie helps analyze web traffic or lets you know when you visit a particular site. Cookies allow web applications to respond to you as an individual. The web application can tailor its operations to your needs, likes and dislikes by gathering and remembering information about your preferences. We use traffic log cookies to identify which pages are being used. This helps us analyze data about webpage traffic and improve our website in order to tailor it to customer needs. We only use this information for statistical analysis purposes and then the data is removed from the system. Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find useful and which you do not. A cookie in no way gives us access to your computer or any information about you, other than the data you choose to share with us. You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can usually modify your browser settings to decline cookies if you prefer. This may prevent you from taking full advantage of the website.</p>
+
+            <h3>LINKS TO OTHER WEBSITES</h3>
+            <p>Our website may contain links to other websites of interest. However, once you have used these links to leave our site, you should note that we do not have any control over that other website. Therefore, we cannot be responsible for the protection and privacy of any information which you provide whilst visiting such sites and such sites are not governed by this privacy statement. You should exercise caution and look at the privacy statement applicable to the website in question.</p>
+
+            <h3>CONTROLLING YOUR PERSONAL INFORMATION</h3>
+            <p>You may choose to restrict the collection or use of your personal information in the following ways:</p>
+
+            <ul>
+                <li>Whenever you are asked to fill in a form on the website, look for the box that you can click to indicate that you do not want the information to be used by anybody for direct marketing purposes.</li>
+                <li>If you have previously agreed to us using your personal information for direct marketing purposes, you may change your mind at any time by writing to or emailing us at privacy@example.com.</li>
+            </ul>
+
+            <p>We will not sell, distribute or lease your personal information to third parties unless we have your permission or are required by law to do so. We may use your personal information to send you promotional information about third parties which we think you may find interesting if you tell us that you wish this to happen. You may request details of personal information which we hold about you under the Data Protection Act 1998. A small fee will be payable. If you would like a copy of the information held on you, please contact us. If you believe that any information, we are holding on you is incorrect or incomplete, please write to or email us as soon as possible, at the above address. We will promptly correct any information found to be incorrect.</p>
+        </div>
+    </div>
+
+    <footer class="mainfooter" id ="footer">
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-9">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="footerWidget web-icon">
+                                <img src="../../images/footer-logo-img.png" alt="footer-logo" class="fadlo"></img>
+                                <br>
+                                   <br>
+                                     
+
+                                <p><strong>Go Freightmate®: Efficient. Reliable. Trusted.</strong><br>
+                                Transforming the logistics landscape, Go Freightmate® connects businesses and drivers with seamless freight management tools. From real-time tracking to secure payments, we're here to make your logistics journey effortless and efficient.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="footerWidget web-list">
+                                <h5>Useful Links</h5>
+                                <ul class="footer-list">
+                                    <li><a href="#">How It Works?</a></li>
+                                    <li><a href="https://gofreightmate.com/privacy-policy">Privacy Policy</a></li>
+                                    <li><a href="https://gofreightmate.com/terms-of-service">Terms of Service</a></li>
+                                    <li><a href="#">Site Map</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="footerWidget web-list">
+                                <h5>Support</h5>
+                                <ul class="footer-list">
+                                    <li><a href="#">FAQs</a></li>
+                                    <li><a href="#">Editor Help</a></li>
+                                    <li><a href="#">Life Chatting</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="footerWidget footerApp">
+                                <div class="heroBtns">
+                                    <a href="javascript:void(0)"><img src="../../images/app-store-icon.png" alt="app-store"></a>
+                                    <a href="javascript:void(0)"><img src="../../images/play-store-icon.png" alt="play-store"></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row bottom-footer">
+                        <div class="col-sm-8">
+                            <div class="bottomLeft">
+                                <span>© 2025 <strong>Go Freightmate.</strong> All Rights Reserved</span><br>
+                                <span>Site design & development by <a href="#">Contemporary Media Group, LLC</a></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 text-end">
+                            <ul class="list-inline info-List">
+                                <li class="list-inline-item"><a href="#">Site Map </a></li>
+                                <li class="list-inline-item"><a href="https://gofreightmate.com/privacy-policy"> Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="footerForm">
+                      <form @submit.prevent="submitForm" class="footerContact">
+                            <h5><img src="../../images/message-icon.png" alt="message-icon"> Quick Contact Form</h5>
+                            <input type="text" name="username" id="username" v-model="form.username"  placeholder="Your Name*" required>
+                            <input type="email" name="email" id="email" v-model="form.email" placeholder="Email Address*" required>
+                            <textarea name="message" id="message" v-model="form.message" placeholder="Your Message ..."></textarea>
+                            <div class="contactSubmit text-end">
+                                <input type="submit" value="Send">
+                            </div>
+                            <p v-if="successMessage" class="success">{{ successMessage }}</p>
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+                      </form> 
+                    </div>
+                </div>
+            </div>
+        </div>
+  </footer>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'PrivacyPolicy',
+  data() {
+    return {
+      form: {
+        username: '',
+        email: '',
+        message: '',
+      },
+      successMessage: "",
+      errorMessage: "",
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await axios.post('/contact', this.form);
+        console.log('Form submitted successfully:', response.data);
+        this.successMessage = "Message sent successfully!";
+        this.errorMessage = "";
+        this.resetForm();
+      } catch (error) {
+        console.error('Error submitting form:', error?.response?.data || error.message);
+        this.errorMessage = "Failed to send message. Please try again.";
+        this.successMessage = "";
+      }
+    },
+    resetForm() {
+      this.form = {
+        username: '',
+        email: '',
+        message: '',
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+header.mainHead {
+  background: #21233c;
+  position: relative;
+  padding: 10px 0;
+}
+footer#footer {
+  background: #f5f5f5;
+}
+h1 {
+  font-size: 32px !important;
+  font-weight: 700;
+}
+h2 {
+  font-size: 24px !important;
+  margin: 0 0 3rem;
+}
+h3 {
+  color: #4e76c7;
+  font-size: 24px !important;
+  margin: 3rem 0 0.5rem;
+}
+h4 {
+  font-size: 18px !important;
+  font-weight: 700;
+  margin: 2rem 0 0.5rem;
+}
+</style>
