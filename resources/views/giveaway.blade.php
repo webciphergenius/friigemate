0efec8aafc93dbdf2b6e2819178da21f6dc5cf05
webciphergenius <lucdev93@gmail.com>
2025-10-14 18:17:13 +0530

    add pdf link

--- resources/views/giveaway.blade.php
+++ resources/views/giveaway.blade.php
@@ -6,6 +6,6 @@
   <title>Go Freightmate</title>
 </head>
 <body style="margin:0; padding:0; text-align: center;">
-  <iframe src="pdf-gofreightmate.pdf" style="width:100%; height:700px;" frameborder="0"></iframe>
+  <iframe src="/pdf-gofreightmate.pdf" style="width:100%; height:100vh;" frameborder="0"></iframe>
 </body>
 </html>
