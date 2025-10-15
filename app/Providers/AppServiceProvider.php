e485dae03a08160ffe05bb764173a7020c54f7e0
webciphergenius <lucdev93@gmail.com>
2025-10-15 23:43:19 +0530

    fixing last image issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
+use App\Models\BlogPost;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
-        //
+        
+        // Bind the blog post model for route model binding
+        Route::model('post', BlogPost::class);
     }
 }
