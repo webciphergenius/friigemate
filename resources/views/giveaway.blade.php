88eea06fd6c576a8c4f12990ef4ba0f609fc8e6a
DesignerNik05 <wd.nikitadhiman@gmail.com>
2025-10-18 12:13:33 +0530

    updated mobile PDF

--- resources/views/giveaway.blade.php
+++ resources/views/giveaway.blade.php
@@ -4,8 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Go Freightmate</title>
+<style>
+.desktoppdf {
+  display: block;
+}
+.mobilepdf {
+  display: none;
+}
+@media only screen and (max-width: 768px) {
+  .desktoppdf {
+    display: none;
+  }
+  .mobilepdf {
+    display: block;
+  }
+}
+</style>
 </head>
 <body style="margin:0; padding:0; text-align: center;">
-  <iframe src="/pdf-gofreightmate.pdf" style="width:100%; height:100vh;" frameborder="0"></iframe>
+  <iframe src="/pdf-gofreightmate.pdf" style="width:100%; height:100vh;" frameborder="0" class="desktoppdf"></iframe>
+  <iframe src="https://contemporarymediagrp.com/wp-content/uploads/2025/10/mobilepdf-screen.pdf" style="width:100%; height:100vh;" frameborder="0" class="mobilepdf"></iframe>
 </body>
 </html>
